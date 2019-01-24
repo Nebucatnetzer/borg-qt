8d7d5ead2d553b3d9e3af22161558ff2a8ab9fb3
Andreas Zweili <andreas@2li.ch>
2019-01-24 21:38:37 +0100

    remove old comments

--- borg_qt/borg_qt.py
+++ borg_qt/borg_qt.py
@@ -6,14 +6,9 @@ import sys
 from main_window import MainWindow
 
 if __name__ == "__main__":
-    # creates the main application, only one of these is every needed
-    # in an application
     app = QApplication(sys.argv)
-    # creates a new window, you can have multiple of these
     window = MainWindow()
-    # show the window, they are hidden by default
     window.show()
     window.config.read()
 
-    # start the application
     sys.exit(app.exec_())
