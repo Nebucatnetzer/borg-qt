258d978a0272a83db9f31ea4025695f6b67693d0
Andreas Zweili <andreas@2li.ch>
2019-01-26 11:25:26 +0100

    move the first config read into a method

--- borg_qt/borg_qt.py
+++ borg_qt/borg_qt.py
@@ -4,16 +4,12 @@ from PyQt5.QtWidgets import QApplication
 import sys
 
 from main_window import MainWindow
-from helper import BorgException, show_error
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    try:
-        window.config.read()
-    except BorgException as e:
-        show_error(e)
-        sys.exit(1)
+    window.start()
 
     sys.exit(app.exec_())
--- borg_qt/main_window.py
+++ borg_qt/main_window.py
@@ -1,10 +1,12 @@
 import os
+import sys
 
 from PyQt5 import uic
 from PyQt5.QtCore import QCoreApplication
 from PyQt5.QtWidgets import QMainWindow
 
 from config import Config
+from helper import BorgException, show_error
 
 
 class MainWindow(QMainWindow):
@@ -19,6 +21,15 @@ class MainWindow(QMainWindow):
         self.config = Config()
 
         self.action_settings.triggered.connect(self.show_settings)
+
+    def start(self):
+        try:
+            self.config.read()
+            self.config._set_environment_variables()
+
+        except BorgException as e:
+            show_error(e)
+            sys.exit(1)
 
     def show_settings(self):
         self.config.set_form_values()
