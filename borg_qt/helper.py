6c9e64f9dc36746ab5c12f2bfeac810ee9276e34
Andreas Zweili <andreas@2li.ch>
2019-01-26 10:40:51 +0100

    add a function to display an error dialog

--- borg_qt/helper.py
+++ borg_qt/helper.py
@@ -1,2 +1,15 @@
+from PyQt5.QtWidgets import QMessageBox
+
+
 class BorgException(Exception):
     pass
+
+
+def show_error(e):
+    # Error Dialog
+    message = QMessageBox()
+    message.setIcon(QMessageBox.Warning)
+    message.setText("Error")
+    message.setWindowTitle("Borg-Qt Error")
+    message.setInformativeText(e.args[0])
+    message.exec_()
