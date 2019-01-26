24f4648bd936a75a4dd96bde32c604b291abc501
Andreas Zweili <andreas@2li.ch>
2019-01-26 10:41:12 +0100

    show error dialog when config file is missing

--- borg_qt/borg_qt.py
+++ borg_qt/borg_qt.py
@@ -4,11 +4,16 @@ from PyQt5.QtWidgets import QApplication
 import sys
 
 from main_window import MainWindow
+from helper import BorgException, show_error
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
-    window.config.read()
+    try:
+        window.config.read()
+    except BorgException as e:
+        show_error(e)
+        sys.exit(1)
 
     sys.exit(app.exec_())
